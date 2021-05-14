47f0602455198e8f096e36d0bacf9f0464b668b4
Ibe Arua <ibe@emplug.com>
2021-05-14 06:26:04 +0100

    feature(oreoswap-website):header implementing header components

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import "../css/local/components/_header.css";
+const Header = () => {
+    return(
+        <header>
+        <img src="img/oreoswap-brown-logo.png" className="oreoswap-logo"/>
+        <ul>
+        <li className="menu-item">
+        <a href="#">Team</a>
+        </li>
+        <li  className="menu-item">
+        <a href="#">Info</a>
+        </li>
+        <li  className="menu-item">
+        <a href="#">Contact</a>
+        </li>
+        <li  className="menu-item">
+        <a href="#">Github</a>
+        </li>
+        <li  className="menu-item">
+        <a href="#">Blog</a>
+        </li>
+        <li  className="menu-item">
+        <a href="#">Team</a>
+        </li>
+        <li  className="menu-item">
+        <a href="#">Team</a>
+        </li>
+        </ul>
+        
+        </header>
+    )
+}
+
+export default Header;
