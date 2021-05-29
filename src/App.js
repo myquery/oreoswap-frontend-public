3ca4ae78f1a2aaa439216edb1b04e7db1ddff102
Olotu Oluwagbemiga <gbenga.olotu@live.com>
2021-05-29 13:07:42 +0100

    commit: updates the App.Js with the card component and references

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ import StatisticSection from "./components/StatisticSection"
 import OptimizedSection from "./components/OPtimizedSection"
 import Newsletter from "./components/Newletter"
 import Footer from "./components/Footer"
+import Card from "./components/Card";
 // icons
 import newsletterDarkBg from "./icons/newsletter-vector-dark.svg"
 import newsletterLightBg from "./icons/newsletter-vector-light.svg"
+import wallet from "./icons/Wallet.svg";
+import reward from "./icons/Reward.svg";
+import medal from "./icons/Medal.svg";
+import integration from "./icons/Integration.svg";
+import improvedPortfolio from "./icons/ImprovedPortfolio.svg";
+import safePaymentGateway from "./icons/SafePaymentGateway.svg";
 
 class App extends Component {
 	state = {
@@ -41,6 +48,39 @@ class App extends Component {
 				<StatisticSection
 				 	isClicked = {this.state.isClicked}
 				/>
+				<div className="cardComponent">
+					<Card
+						isClicked={this.state.isClicked}
+						cardTitle="Personal Wallet"
+						cardImage={wallet}
+					/>
+					<Card
+						isClicked={this.state.isClicked}
+						cardTitle="Liquidity Portal"
+						cardImage={medal}
+					/>
+					<Card
+						isClicked={this.state.isClicked}
+						cardTitle="Improved Portfolio"
+						cardImage={improvedPortfolio}
+					/>
+					<Card
+						isClicked={this.state.isClicked}
+						cardTitle="Reward System"
+						cardImage={reward}
+					/>
+					<Card
+						isClicked={this.state.isClicked}
+						cardTitle="Stepwise Integration"
+						cardImage={integration}
+					/>
+					<Card
+						isClicked={this.state.isClicked}
+						cardTitle="Safe Payment Gateway"
+						cardImage={safePaymentGateway}
+					/>
+				</div>
+
 				<OptimizedSection
 					toggleMode = {toggleMode}
 					isClicked = {this.state.isClicked}
