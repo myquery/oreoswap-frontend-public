13bc2989122904bdb127786c9484a4e0e8edcc59
user <ojemehprogress@gmail.com>
2021-06-09 13:50:55 +0100

    fix responsiveness bug

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,14 +9,20 @@ const Banner = ({isClicked}) => {
 						The New All Powerful Decentralized Trading Protocol
 					</h1>
 					<p className="">An Automated Market Maker and DApps guaranteed for speed and low fees transactions.</p>
-					<div className="d-flex flex-column flex-md-row flex-lg-column flex-xl-row banner-button-wrapper">
-						<button className="banner-button button-active"><a href="#">Use Exchange</a></button>
-						<button className={isClicked ? "banner-button banner-button-darkmode" : "banner-button banner-button-lightmode"}><a href="#">Documentation</a></button>
-						<button className={isClicked ? "banner-button banner-button-darkmode" : "banner-button banner-button-lightmode"}><a href="#">FAQ</a></button>
+					<div className=" banner-button-wrapper">
+						<button className="banner-button button-active">
+							<a href="#">Use Exchange</a>
+						</button>
+						<button className={isClicked ? "banner-button banner-button-darkmode" : "banner-button banner-button-lightmode"}>
+							<a href="#">Documentation</a>
+						</button>
+						<button className={isClicked ? "banner-button banner-button-darkmode" : "banner-button banner-button-lightmode"}>
+							<a href="#">FAQ</a>
+						</button>
 					</div>
 				</div>
 				<div className="banner-img-wrapper">
-					<img src="img/banner-img.png" alt="oreo bisscuit" className="banner-img"/>
+					<img src="img/banner-img.png" alt="oreo biscuit" className="banner-img"/>
 				</div>
 			</div>
 		</section>
