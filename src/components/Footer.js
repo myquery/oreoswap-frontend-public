3c09e5b0d127ff8b78aa477c1163e4bed6dbc3c0
ekomic <ekomchris2016@gmail.com>
2021-08-08 13:23:52 +0100

    implemented footer for desktop view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,45 @@
+import React from 'react'
 import "../css/local/partials/components/_footer.css"
 
-const Footer = ({isClicked}) => {
-	return(
-		<footer className={isClicked ? "footer-darkmode" : "footer-lightmode"}>
-			<nav className="wrapper">	
-				<ul className="footer-nav list-unstyled d-flex">
-					<li className="m-0">&copy; 2021 OreoSwap</li>
-					<li className="ml-auto"><a href="">About</a></li>
-					<li><a href="">Disclaimer</a></li>
-					<li><a href="">Trademark</a></li>
-					<li><a href="">Brand</a></li>
-				</ul>
-			</nav>
-		</footer>
+
+function Footer() {
+	return (
+		<div className="footer">
+            <div className="line"></div>
+			 <div className="footer__main">
+            	<div className="footer__left">
+                	<p className="footer__leftHeading">We do apprecite your feedback</p>
+                	<p className="footer__leftSubHeading">hello@oreoswap.com</p>
+            	</div>
+
+            <div className="footer__right">
+                <p className="footer__rightHeading">OreoSwap directly in your inbox</p>
+                <p className="footer__rightSubHeading">Get weekly updates about OreoSwap before anyone</p>
+
+                <div className="footer__search">
+                    <input className="search" type="text" placeholder="Enter Your Email" />
+                    <button className="footer__btn">Subscribe</button>
+                </div>
+            </div>
+        </div>
+
+		<div className="footer__bottom">
+            <a className="footer__link">&copy; 2021 OreoSwap</a>
+
+            <ul className="footer__list">
+                <li className="footer_listItem"><a className="footer_link" href="">Team</a></li>
+                <li className="footer_listItem"><a className="footer_link" href="">Info</a></li>
+                <li className="footer_listItem"><a className="footer_link" href="">Services</a></li>
+                <li className="footer_listItem"><a className="footer_link" href="">Github</a></li>
+                <li className="footer_listItem"><a className="footer_link" href="">Blog</a></li>
+            </ul>
+        </div>
+		</div>
+			
+		
 	)
 }
 
 export default Footer
+
+
