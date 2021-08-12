59f2e1da9c2fbde52504c5ae8a9a3c3370d56bd8
Decagon <e@Mzee.local>
2021-08-12 14:53:46 -0700

    Hero Section Implemented for both the light and darkMode

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,15 +3,15 @@ import "../css/local/partials/components/_banner.css"
 const Banner = ({isClicked}) => {
 	return(
 		<section className={isClicked ? "banner banner-bg-darkmode" : "banner banner-bg-lightmode"}>
-			<div className="wrapper banner-container">
-				<div className="">
-					<h1 className="">
-						The New All Powerful Decentralized Trading Protocol
+			<div className=" wrapper banner-container">
+				<div className="child">
+					<h1 className="hero-wrapper">
+					Super-charged DeFi Exchange 
 					</h1>
-					<p className="">An Automated Market Maker and DApps guaranteed for speed and low fees transactions.</p>
-					<div className=" banner-button-wrapper">
-						<button className="banner-button button-active">
-							<a href="#">Use Exchange</a>
+					<p className="">An Automated Market Maker and DApps guaranteed for speed <br/>and low fees transactions.</p>
+					<div className=" center banner-button-wrapper">
+						<button className="small banner-button button-active">
+							<a id="smallText" href="#">Use Exchange</a>
 						</button>
 						<button className={isClicked ? "banner-button banner-button-darkmode" : "banner-button banner-button-lightmode"}>
 							<a href="#">Documentation</a>
@@ -20,9 +20,6 @@ const Banner = ({isClicked}) => {
 							<a href="#">FAQ</a>
 						</button>
 					</div>
-				</div>
-				<div className="banner-img-wrapper">
-					<img src="img/banner-img.png" alt="oreo biscuit" className="banner-img"/>
 				</div>
 			</div>
 		</section>
