d4ae4acf349af2b10d2b680ba8ccd58fd4be7a75
Christopher Ekom <ekomchris2016@gmail.com>
2021-08-26 05:37:05 -0700

    implemented hero-section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,28 +1,19 @@
 import "../css/local/partials/components/_banner.css"
 
-const Banner = ({isClicked}) => {
+const Banner = () => {
 	return(
-		<section className={isClicked ? "banner banner-bg-darkmode" : "banner banner-bg-lightmode"}>
-			<div className="wrapper banner-container">
-				<div className="">
-					<h1 className="">
-						The New All Powerful Decentralized Trading Protocol
-					</h1>
-					<p className="">An Automated Market Maker and DApps guaranteed for speed and low fees transactions.</p>
-					<div className=" banner-button-wrapper">
-						<button className="banner-button button-active">
-							<a href="#">Use Exchange</a>
-						</button>
-						<button className={isClicked ? "banner-button banner-button-darkmode" : "banner-button banner-button-lightmode"}>
-							<a href="#">Documentation</a>
-						</button>
-						<button className={isClicked ? "banner-button banner-button-darkmode" : "banner-button banner-button-lightmode"}>
-							<a href="#">FAQ</a>
-						</button>
-					</div>
+		<section className="banner">
+			<div className="hero">
+				<h1 className="topHeading">Super-charged DeFi Exchange</h1>
+				<div className="heroSubHeading">
+					<p className="subHeading">
+						An Automated Market Maker and DApps guaranteed for speed and low fees transactions.
+					</p>
 				</div>
-				<div className="banner-img-wrapper">
-					<img src="img/banner-img.png" alt="oreo biscuit" className="banner-img"/>
+				<div className="buttons">
+					<a href="#" className="btn smallBtn" type="button">Use Exchange</a>
+					<a href="#" className="btn largeBtn" type="button">Documention</a>
+					<a href="#" className="btn mediumBtn" type="button">FAQs</a>
 				</div>
 			</div>
 		</section>
